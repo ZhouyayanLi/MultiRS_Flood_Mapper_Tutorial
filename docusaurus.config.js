62bb906c944ce452a79201ce1b9c7e4abf3baffe
Zhouyayan (Iris) Li <145369690+ZhouyayanLi@users.noreply.github.com>
2023-12-18 13:50:19 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,11 @@ const config = {
   favicon: 'img/uihilab.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://zhouyayanli.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/MultiRS_Flood_Mapper/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
