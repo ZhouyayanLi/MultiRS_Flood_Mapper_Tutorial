32ad5269983cf9a859c75cb1589170b90a8b5190
Yusuf Sermet <muhammedyusuf-sermet@uiowa.edu>
2023-12-19 14:26:28 -0600

    Modify baseURL for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://zhouyayanli.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/MultiRS_Flood_Mapper/',
+  baseUrl: '/tutorials/floodmapper/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
